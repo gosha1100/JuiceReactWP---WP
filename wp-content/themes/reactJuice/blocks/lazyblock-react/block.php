869606c3d5e1b695517c72b8c7c5fb34dcd4337c
Julian Strohmeier <julian.strohmeier@protonmail.com>
2023-10-25 15:12:34 +0200

    refactor: lazyblock repeater example added

--- wp-content/themes/reactJuice/blocks/lazyblock-react/block.php
+++ wp-content/themes/reactJuice/blocks/lazyblock-react/block.php
@@ -1,6 +1,24 @@
 <?php
+
+$repeater_items = array();
+
+if (isset($attributes['repeater']) && is_array($attributes['repeater'])) {
+    foreach ($attributes['repeater'] as $item) {
+        $title = isset($item['title']) ? $item['title'] : null;
+        $image_url = isset($item['image']['url']) ? $item['image']['url'] : null;
+
+        if ($title !== null && $image_url !== null) {
+            $repeater_items[] = array(
+                'title' => $title,
+                'url' => $image_url
+            );
+        }
+    }
+}
+
 $destructuredJuicer = [
-    'headline' => $attributes['headline']
+    'headline' => $attributes['headline'],
+    'items' => $repeater_items,
 ];
 
 $encodedJuicer = json_encode($destructuredJuicer);
